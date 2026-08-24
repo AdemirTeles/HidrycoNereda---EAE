81ef17831e8837e7cede3ebe7fa03a1d184dae66
ademir-teles <ademir.teles@se.com>
2026-08-24 16:02:46 -0300

    CompressorsTags

--- project/SE.Nereda/HMI/Compressor/Compressor.event.cs
+++ project/SE.Nereda/HMI/Compressor/Compressor.event.cs
@@ -10,5 +10,191 @@ using NxtControl.GuiFramework;
 using NxtControl.Services;
 
 #region Definitions;
+#region #Compressor_HMI;
+
+namespace SE.Nereda.Symbols.Compressor
+{
+
+  public class FB_STATUSEventArgs : System.EventArgs
+  {
+    IHMIAccessorService accessorService;
+    int channelId;
+    int cookie; 
+    int eventIndex;
+
+    public FB_STATUSEventArgs(int channelId, int cookie, int eventIndex)
+    {
+      this.accessorService = (IHMIAccessorService)ServiceProvider.GetService(typeof(IHMIAccessorService));
+      this.channelId = channelId;
+      this.cookie = cookie;
+      this.eventIndex = eventIndex;
+    }
+    public bool Get_Run(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,0, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Run
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,0, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+    public bool Get_Alarm(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,1, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Alarm
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,1, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+    public bool Get_Mode(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,2, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Mode
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,2, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+    public bool Get_Frequency(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,3, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Frequency
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,3, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+    public bool Get_Power(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,4, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Power
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,4, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+    public bool Get_Capacity(ref System.Int16 value)
+    {
+      if (accessorService == null)
+        return false;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,5, ref var);
+      if (ret) value = (System.Int16) var;
+      return ret;
+    }
+
+    public System.Int16? Capacity
+    { get {
+      if (accessorService == null)
+        return null;
+      System.Int64 var = 0;
+      bool ret = accessorService.GetInt64Value(channelId, cookie, eventIndex, true,5, ref var);
+      if (!ret) return null;
+      return (System.Int16) var;
+    }  }
+
+
+  }
+
+}
+
+namespace SE.Nereda.Symbols.Compressor
+{
+  partial class sDefault
+  {
+
+    private event EventHandler<SE.Nereda.Symbols.Compressor.FB_STATUSEventArgs> FB_STATUS_Fired;
+
+    protected override void OnEndInit()
+    {
+      if (FB_STATUS_Fired != null)
+        AttachEventInput(0);
+
+    }
+
+    protected override void FireEventCallback(int channelId, int cookie, int eventIndex)
+    {
+      switch(eventIndex)
+      {
+        default:
+          break;
+        case 0:
+          if (FB_STATUS_Fired != null)
+          {
+            try
+            {
+              FB_STATUS_Fired(this, new SE.Nereda.Symbols.Compressor.FB_STATUSEventArgs(channelId, cookie, eventIndex));
+            }
+            catch (System.Exception e)
+            {
+              NxtControl.Services.LoggingService.ErrorFormatted(@"In Event Callback for event:'{0}' Type:'{1}' CAT:'{2}' came exception:{3}
+stack Trace:
+{4}","FB_STATUS_Fired", this.GetType().Name, this.CATName, e.Message, e.StackTrace);
+            }
+          }
+        break; 
+
+      }
+    }
+
+  }
+}
+#endregion #Compressor_HMI;
 
 #endregion Definitions;
--- project/SE.Nereda/HMI/ReactorLogic/ReactorLogic.event.cs
+++ project/SE.Nereda/HMI/ReactorLogic/ReactorLogic.event.cs
@@ -10,5 +10,71 @@ using NxtControl.GuiFramework;
 using NxtControl.Services;
 
 #region Definitions;
+#region #ReactorLogic_HMI;
+
+namespace SE.Nereda.Symbols.ReactorLogic
+{
+
+  public class CNFEventArgs : System.EventArgs
+  {
+    IHMIAccessorService accessorService;
+    int channelId;
+    int cookie; 
+    int eventIndex;
+
+    public CNFEventArgs(int channelId, int cookie, int eventIndex)
+    {
+      this.accessorService = (IHMIAccessorService)ServiceProvider.GetService(typeof(IHMIAccessorService));
+      this.channelId = channelId;
+      this.cookie = cookie;
+      this.eventIndex = eventIndex;
+    }
+
+  }
+
+}
+
+namespace SE.Nereda.Symbols.ReactorLogic
+{
+  partial class sDefault
+  {
+
+    private event EventHandler<SE.Nereda.Symbols.ReactorLogic.CNFEventArgs> CNF_Fired;
+
+    protected override void OnEndInit()
+    {
+      if (CNF_Fired != null)
+        AttachEventInput(0);
+
+    }
+
+    protected override void FireEventCallback(int channelId, int cookie, int eventIndex)
+    {
+      switch(eventIndex)
+      {
+        default:
+          break;
+        case 0:
+          if (CNF_Fired != null)
+          {
+            try
+            {
+              CNF_Fired(this, new SE.Nereda.Symbols.ReactorLogic.CNFEventArgs(channelId, cookie, eventIndex));
+            }
+            catch (System.Exception e)
+            {
+              NxtControl.Services.LoggingService.ErrorFormatted(@"In Event Callback for event:'{0}' Type:'{1}' CAT:'{2}' came exception:{3}
+stack Trace:
+{4}","CNF_Fired", this.GetType().Name, this.CATName, e.Message, e.StackTrace);
+            }
+          }
+        break; 
+
+      }
+    }
+
+  }
+}
+#endregion #ReactorLogic_HMI;
 
 #endregion Definitions;
