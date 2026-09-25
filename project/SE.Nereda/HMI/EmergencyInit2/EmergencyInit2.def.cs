06bb575ee58d52e96589034af5b844bf21fe8d49
Ary Viana Mariano <ary.vm@hotmail.com>
2026-09-25 08:29:11 -0300

    Add Blocks

---
+++ project/SE.Nereda/HMI/EmergencyInit2/EmergencyInit2.def.cs
@@ -0,0 +1,11 @@
+using System;
+using NxtControl.GuiFramework;
+using NxtControl.Services;
+
+
+#region Definitions;
+#region EmergencyInit2_HMI;
+#endregion EmergencyInit2_HMI;
+
+#endregion Definitions;
+
